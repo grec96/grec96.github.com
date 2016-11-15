2977658208239d9aa28056439b12f5757044af05
grec96 <zelic96@gmail.com>
2016-11-15 23:52:06 +0300

    Valid email

--- js/send.js
+++ js/send.js
@@ -2,10 +2,12 @@ function ChkState() {
     var name = $('#posName').val();
     var email = $('#posEmail').val();
     var text = $('#posText').val();
+    var r = new RegExp(/.+@.+\..+/i)
 
-    if(name.length >= 3 && email.length != 0 && text.length != 0)
-    {
-        $('#send').removeAttr('disabled');
+
+        if (r.test($('#posEmail').val()) && name.length >= 3 && text.length !=0)
+        {
+            $('#send').removeAttr('disabled');
     }
     else
     {
